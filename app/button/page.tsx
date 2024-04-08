16a0589a3ee56a00d705c54b57fbdd9e21d77e94
zuoqian <zuoqian@travelsky.com.cn>
2024-04-08 16:42:00 +0800

    修改button样式，新增button路由页面

---
+++ app/button/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>default</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="primary">primary</Button>
+    </div>
+  
+  );
+}
