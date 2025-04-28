e7303c33d5ed6f007c28a3a7aa8d71ad1977d21b
Fomalhaut00 <317058190@qq.com>
2025-04-28 22:24:37 +0800

    feat: marketing page

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,7 +1,19 @@
-export function Header(){
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export const Header = () => {
   return (
-    <header className=" h-20 w-full border-b-2 border-slate-200 px-4">
-      header
+    <header className="h-20 w-full border-b-2 border-slate-200 px-4">
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" height={40} width={40} alt="Mascot" />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+          <button >signin</button>
+      </div>
     </header>
-  )
-}
+  );
+};
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,14 +1,20 @@
-import {Header} from "./header";
 import { Footer } from "./footer";
-function MarketingLayout({ children }: { children: React.ReactNode }) {
+import { Header } from "./header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header/>
-      <main className=" flex-1 flex flex-col justify-center items-center">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center">
         {children}
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
-}
+};
+
 export default MarketingLayout;
